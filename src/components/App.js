9db946d928e614ed0f19311fc43d3402fa0b22f4
utkarshabakshi <utkarsha.bakshi@gmail.com>
2020-08-05 04:59:02 -0400

    information header in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import InfoHead from './InfoHead';
 
 class App extends React.Component{
     render(){
         return(
-            <div> Hello World</div>
+            <InfoHead/>
         );
     }
 };
