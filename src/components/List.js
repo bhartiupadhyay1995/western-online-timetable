427fe7db5ea977e8b66424d2a5f9e7ef5989fa76
utkarshabakshi <utkarsha.bakshi@gmail.com>
2020-08-14 07:19:02 -0400

    demo template to retrieve data from local storage done

---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class List extends React.Component{
+    coursesFromStorage = window.localStorage.getItem('SAVED_COURSES');
+    render(){
+        return <div></div>
+    }
+}
+
+export default{
+    List
+}
